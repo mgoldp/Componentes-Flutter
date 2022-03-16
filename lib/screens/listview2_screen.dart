02d93ca54fd6a6e52cbaad53d7684d58aaa41e47
mgoldp <mgoldp2013@gmail.com>
2022-03-16 13:37:07 -0300

    Taps sobre los ListTiles y estilo del AppBar

--- lib/screens/listview2_screen.dart
+++ lib/screens/listview2_screen.dart
@@ -15,12 +15,16 @@ class ListView2Screen extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
           title: const Text('ListView Tipo 2'),
+          elevation: 0,
+          backgroundColor: Colors.indigo,
         ),
         body: ListView.separated(
           itemCount: options.length,
           itemBuilder: (context, index) => ListTile(
             title: Text(options[index]),
-            trailing: const Icon(Icons.arrow_forward_ios_outlined),
+            trailing: const Icon(Icons.arrow_forward_ios_outlined,
+                color: Colors.indigo),
+            onTap: null,
           ),
           separatorBuilder: (_, __) => const Divider(),
         ));
