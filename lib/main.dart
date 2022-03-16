cd58f4f2b9fc9addad15e8310c9acbe132ae6189
mgoldp <mgoldp2013@gmail.com>
2022-03-16 13:12:47 -0300

    ListView: Builder y Separated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:fl_components/screens/listview1_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:fl_components/screens/listview2_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Material App',
-      home: ListView1Screen(),
+      home: ListView2Screen(),
       debugShowCheckedModeBanner: false,
     );
   }
---
+++ lib/screens/listview2_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class ListView2Screen extends StatelessWidget {
+  const ListView2Screen({Key? key}) : super(key: key);
+
+  final options = const [
+    'Megaman',
+    'Metal Gear',
+    'Super Smash',
+    'Final Fantasy'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('ListView Tipo 2'),
+        ),
+        body: ListView.separated(
+          itemCount: options.length,
+          itemBuilder: (context, index) => ListTile(
+            title: Text(options[index]),
+          ),
+          separatorBuilder: (_, __) => const Divider(),
+        ));
+  }
+}
