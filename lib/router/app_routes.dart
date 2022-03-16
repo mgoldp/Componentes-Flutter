5aec34922c7dc74a195523e0055bc2ba173c4fcf
mgoldp <mgoldp2013@gmail.com>
2022-03-16 18:22:42 -0300

    Modelo y Listado de Rutas

--- lib/router/app_routes.dart
+++ lib/router/app_routes.dart
@@ -1,8 +1,38 @@
+import 'package:fl_components/models/models.dart';
 import 'package:fl_components/screens/screens.dart';
 import 'package:flutter/material.dart';
 
 class AppRoutes {
   static const initialRoute = 'home';
+
+  static final menuOptions = <MenuOption>[
+    //TODO: borra home
+    MenuOption(
+        route: 'home',
+        name: 'HomeScreen',
+        screen: const HomeScreen(),
+        icon: Icons.home),
+    MenuOption(
+        route: 'lisview1',
+        name: 'ListView1',
+        screen: const ListView1Screen(),
+        icon: Icons.list),
+    MenuOption(
+        route: 'lisview2',
+        name: 'ListView2',
+        screen: const ListView2Screen(),
+        icon: Icons.list),
+    MenuOption(
+        route: 'alert',
+        name: 'Alert',
+        screen: const AlertScreen(),
+        icon: Icons.error),
+    MenuOption(
+        route: 'card',
+        name: 'Card',
+        screen: const CardScreen(),
+        icon: Icons.card_membership),
+  ];
 
   static Map<String, Widget Function(BuildContext)> routes = {
     'listview1': (BuildContext context) => const ListView1Screen(),
