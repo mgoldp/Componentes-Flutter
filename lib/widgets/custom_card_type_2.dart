1231a08fa42c1e898d638681431dff38e757f7d0
mgoldp <mgoldp2013@gmail.com>
2022-03-21 17:44:22 -0300

    Ajustar tamaño de la imagen

--- lib/widgets/custom_card_type_2.dart
+++ lib/widgets/custom_card_type_2.dart
@@ -1,3 +1,4 @@
+import 'package:fl_components/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class CustomCardType2 extends StatelessWidget {
@@ -6,13 +7,24 @@ class CustomCardType2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      clipBehavior: Clip.antiAlias,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
       elevation: 10,
+      shadowColor: AppTheme.primary.withOpacity(0.5),
       child: Column(
-        children: const [
-          FadeInImage(
+        children: [
+          const FadeInImage(
             image: NetworkImage('https://wallpaperaccess.com/full/1289981.jpg'),
             placeholder: AssetImage('assets/jar-loading.gif'),
+            width: double.infinity,
+            height: 260,
+            fit: BoxFit.cover,
+            fadeInDuration: Duration(milliseconds: 300),
           ),
+          Container(
+              alignment: AlignmentDirectional.centerEnd,
+              padding: const EdgeInsets.only(right: 20, top: 10, bottom: 10),
+              child: const Text('Un Hermoso Paisaje')),
         ],
       ),
     );
