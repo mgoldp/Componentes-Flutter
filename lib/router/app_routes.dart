a6bd2bbb69dfef9f7f6e204d797e129b8298f7ae
mgoldp <mgoldp2013@gmail.com>
2022-03-24 19:21:45 -0300

    Widget Container - Configuración sin animar

--- lib/router/app_routes.dart
+++ lib/router/app_routes.dart
@@ -37,6 +37,11 @@ class AppRoutes {
         name: 'Circle Avatar',
         screen: const AvatarScreen(),
         icon: Icons.supervised_user_circle_outlined),
+    MenuOption(
+        route: 'animated',
+        name: 'Animated Container',
+        screen: const AnimatedScreen(),
+        icon: Icons.play_circle_outlined),
   ];
 
   static Map<String, Widget Function(BuildContext)> getAppRoutes() {
