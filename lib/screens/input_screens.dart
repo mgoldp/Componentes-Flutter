9152086bf77dbcbe7b1f617a6153fc7680835887
mgoldp <mgoldp2013@gmail.com>
2022-03-28 12:38:27 -0300

    TextFormFields Personalización

--- lib/screens/input_screens.dart
+++ lib/screens/input_screens.dart
@@ -7,7 +7,7 @@ class InputScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Inputs y Forms'),
+        title: const Text('Inputs y Forms'),
       ),
       body: SingleChildScrollView(
         child: Padding(
@@ -21,6 +21,32 @@ class InputScreen extends StatelessWidget {
                 onChanged: (value) {
                   print('value: $value');
                 },
+                validator: (value) {
+                  if (value == null) return 'Este campo es requerido';
+                  return value.length < 3 ? 'Mínimo 3 letras' : null;
+                },
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                decoration: const InputDecoration(
+                  hintText: 'Nombre del Usuario',
+                  labelText: 'Nombre',
+                  helperText: 'Solo letras',
+                  //prefixIcon: Icon(Icons.supervised_user_circle),
+                  suffixIcon: Icon(Icons.group_outlined),
+                  icon: Icon(Icons.verified_user_sharp),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.green,
+                    ),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(10),
+                      bottomRight: Radius.circular(10),
+                      topRight: Radius.circular(10),
+                      topLeft: Radius.circular(10),
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
