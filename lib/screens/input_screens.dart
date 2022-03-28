dd9ad119cb24498f07f5b1ac3c69d69b84368342
mgoldp <mgoldp2013@gmail.com>
2022-03-28 14:03:45 -0300

    Centralizar el estilo de los inputs

--- lib/screens/input_screens.dart
+++ lib/screens/input_screens.dart
@@ -1,3 +1,4 @@
+import 'package:fl_components/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class InputScreen extends StatelessWidget {
@@ -13,40 +14,10 @@ class InputScreen extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
           child: Column(
-            children: [
-              TextFormField(
-                autofocus: true,
-                initialValue: '',
-                textCapitalization: TextCapitalization.words,
-                onChanged: (value) {
-                  print('value: $value');
-                },
-                validator: (value) {
-                  if (value == null) return 'Este campo es requerido';
-                  return value.length < 3 ? 'Mínimo 3 letras' : null;
-                },
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                decoration: const InputDecoration(
-                  hintText: 'Nombre del Usuario',
-                  labelText: 'Nombre',
-                  helperText: 'Solo letras',
-                  //prefixIcon: Icon(Icons.supervised_user_circle),
-                  suffixIcon: Icon(Icons.group_outlined),
-                  icon: Icon(Icons.verified_user_sharp),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.green,
-                    ),
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(10),
-                      bottomRight: Radius.circular(10),
-                      topRight: Radius.circular(10),
-                      topLeft: Radius.circular(10),
-                    ),
-                  ),
-                ),
+            children: const [
+              CustomInputField(
+                labelText: 'Nombre',
+                hintText: 'Nombre del Usuario',
               ),
             ],
           ),
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -4,32 +4,68 @@ class AppTheme {
   static const Color primary = Colors.indigo;
 
   static final ThemeData lightTheme = ThemeData.light().copyWith(
-      //color primario
-      primaryColor: primary,
+    //color primario
+    primaryColor: primary,
 
-      //AppBar Theme
-      appBarTheme: const AppBarTheme(
-        color: primary,
+    //AppBar Theme
+    appBarTheme: const AppBarTheme(
+      color: primary,
+      elevation: 0,
+    ),
+
+    //TextButton Theme
+    textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(primary: primary),
+    ),
+
+    //FloatingActionButton Theme
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: primary,
+      elevation: 10,
+    ),
+
+    //ElevatedButton Theme
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        primary: primary,
+        shape: const StadiumBorder(),
         elevation: 0,
       ),
+    ),
 
-      //TextButton Theme
-      textButtonTheme: TextButtonThemeData(
-        style: TextButton.styleFrom(primary: primary),
-      ),
-
-      //FloatingActionButton Theme
-      floatingActionButtonTheme: const FloatingActionButtonThemeData(
-        backgroundColor: primary,
-        elevation: 10,
-      ),
-
-      //ElevatedButton Theme
-      elevatedButtonTheme: ElevatedButtonThemeData(
-        style: ElevatedButton.styleFrom(
-          primary: primary,
-          shape: const StadiumBorder(),
-          elevation: 0,
+    //TextFormField Theme
+    inputDecorationTheme: const InputDecorationTheme(
+      floatingLabelStyle: TextStyle(color: primary),
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: primary,
         ),
-      ));
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(10),
+          bottomRight: Radius.circular(10),
+          topLeft: Radius.circular(10),
+          topRight: Radius.circular(10),
+        ),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: primary,
+        ),
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(10),
+          bottomRight: Radius.circular(10),
+          topLeft: Radius.circular(10),
+          topRight: Radius.circular(10),
+        ),
+      ),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(10),
+          bottomRight: Radius.circular(10),
+          topLeft: Radius.circular(10),
+          topRight: Radius.circular(10),
+        ),
+      ),
+    ),
+  );
 }
