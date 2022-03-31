3c190155c5e2b35df079666d7d5a9f6ad0b50d9f
mgoldp <mgoldp2013@gmail.com>
2022-03-30 22:07:18 -0300

    Checkboxes & Switches

--- lib/screens/slider_screen.dart
+++ lib/screens/slider_screen.dart
@@ -10,6 +10,7 @@ class SliderScreen extends StatefulWidget {
 
 class _SliderScreenState extends State<SliderScreen> {
   double _sliderValue = 100;
+  bool _sliderEnabled = true;
 
   @override
   Widget build(BuildContext context) {
@@ -17,29 +18,60 @@ class _SliderScreenState extends State<SliderScreen> {
       appBar: AppBar(
         title: const Text('Slider & Checks'),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Slider.adaptive(
-                min: 50,
-                max: 400,
-                activeColor: AppTheme.primary,
-                value: _sliderValue,
-                onChanged: (value) {
-                  _sliderValue = value;
-                  setState(() {});
-                }),
-            Image(
-              image: const NetworkImage(
-                  'https://w7.pngwing.com/pngs/502/54/png-transparent-marvel-vs-capcom-infinite-marvel-vs-capcom-3-fate-of-two-worlds-iron-man-ultimate-marvel-vs-capcom-3-carol-danvers-others-superhero-fictional-character-iron-thumbnail.png'),
-              fit: BoxFit.contain,
-              width: _sliderValue,
+      body: Column(
+        children: [
+          Slider.adaptive(
+            min: 50,
+            max: 400,
+            activeColor: AppTheme.primary,
+            value: _sliderValue,
+            onChanged: _sliderEnabled
+                ? (value) {
+                    _sliderValue = value;
+                    setState(() {});
+                  }
+                : null,
+          ),
+          // Checkbox(
+          //     value: _sliderEnabled,
+          //     onChanged: (value) {
+          //       _sliderEnabled = value ?? true;
+          //       setState(() {});
+          //     }),
+          CheckboxListTile(
+            activeColor: AppTheme.primary,
+            title: const Text('Habilitar Slider'),
+            value: _sliderEnabled,
+            onChanged: (value) => setState(() {
+              _sliderEnabled = value ?? true;
+            }),
+          ),
+          // Switch(
+          //   value: _sliderEnabled,
+          //   onChanged: (value) => setState(() {
+          //     _sliderEnabled = value;
+          //   }),
+          // ),
+          SwitchListTile(
+            activeColor: AppTheme.primary,
+            title: const Text('Habilitar Slider'),
+            value: _sliderEnabled,
+            onChanged: (value) => setState(() {
+              _sliderEnabled = value;
+            }),
+          ),
+          const AboutListTile(),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Image(
+                image: const NetworkImage(
+                    'https://w7.pngwing.com/pngs/502/54/png-transparent-marvel-vs-capcom-infinite-marvel-vs-capcom-3-fate-of-two-worlds-iron-man-ultimate-marvel-vs-capcom-3-carol-danvers-others-superhero-fictional-character-iron-thumbnail.png'),
+                fit: BoxFit.contain,
+                width: _sliderValue,
+              ),
             ),
-            const SizedBox(
-              height: 100,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
