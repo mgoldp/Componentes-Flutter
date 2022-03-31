452815644e3d50071120e2f91c2fd8b8c8ce8bd5
mgoldp <mgoldp2013@gmail.com>
2022-03-31 16:27:36 -0300

    infinite Scroll

--- lib/screens/listview_builder_screen.dart
+++ lib/screens/listview_builder_screen.dart
@@ -1,7 +1,35 @@
 import 'package:flutter/material.dart';
 
-class ListViewBuilderScreen extends StatelessWidget {
+class ListViewBuilderScreen extends StatefulWidget {
   const ListViewBuilderScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ListViewBuilderScreen> createState() => _ListViewBuilderScreenState();
+}
+
+class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
+  final List<int> imagesIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  final ScrollController scrollControler = ScrollController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    scrollControler.addListener(() {
+      //print('${scrollControler.position.pixels}, ${scrollControler.position.maxScrollExtent}');
+      if ((scrollControler.position.pixels + 500) >=
+          scrollControler.position.maxScrollExtent) {
+        add5();
+      }
+    });
+  }
+
+  void add5() {
+    final lastId = imagesIds.last;
+    imagesIds.addAll([1, 2, 3, 4, 5].map((e) => lastId + e));
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +40,9 @@ class ListViewBuilderScreen extends StatelessWidget {
         removeTop: true,
         removeBottom: true,
         child: ListView.builder(
-            itemCount: 10,
+            physics: const BouncingScrollPhysics(),
+            controller: scrollControler,
+            itemCount: imagesIds.length,
             itemBuilder: (BuildContext context, int index) {
               return FadeInImage(
                 width: double.infinity,
@@ -20,7 +50,7 @@ class ListViewBuilderScreen extends StatelessWidget {
                 fit: BoxFit.cover,
                 placeholder: const AssetImage('assets/jar-loading.gif'),
                 image: NetworkImage(
-                    'https://picsum.photos/500/300?image=${index + 1}'),
+                    'https://picsum.photos/500/300?image=${imagesIds[index]}'),
               );
             }),
       ),
